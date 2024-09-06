3555005229ad245ca437e847267afb88458cd868
D3vle0 <44903787+D3vle0@users.noreply.github.com>
2024-09-06 11:34:16 +0900

    fix: encrypt pw

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const crypto = require('crypto');
 const app = express();
 const router = express.Router();
 
@@ -27,20 +28,19 @@ router.get('/login', (req, res) => {
 
 router.post('/register', (req, res) => {
     const { user_id, user_name, pw } = req.body;
-    if (!user_id || !user_name || !pw) {
+    if (!user_id || !user_name || !pw)
         return res.status(400).json({ error: '필수 파라미터 값이 누락되었습니다.' });
-    }
 
     // 중복된 user_id 확인
     Account.findOne({ user_id })
         .then(existingAccount => {
             if (existingAccount)
-                return res.status(400).json({ error: '중복된 ID 입니다.' }); // Ensure to return here
+                return res.status(400).json({ error: '중복된 ID 입니다.' })
             return new Account({
                 user_id,
                 user_name,
                 created_at: new Date(),
-                password: pw,
+                password: crypto.createHash('sha256').update(pw).digest('hex')
             }).save();
         })
         .then(savedAccount => res.status(201).json({ message: '계정이 생성되었습니다.', account: savedAccount }))
