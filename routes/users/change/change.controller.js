d2417f0029e402d39af46e750eba2f9ecc50ac07
D3vle0 <44903787+D3vle0@users.noreply.github.com>
2024-10-09 20:49:51 +0900

    feat: change password

--- routes/users/change/change.controller.js
+++ routes/users/change/change.controller.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 const Account = require('../../../db/account');
 
 exports.username = (req, res) => {
@@ -20,6 +21,37 @@ exports.username = (req, res) => {
       .catch(err => {
         if (!res.headersSent)
           return res.status(500).json({ error: '사용자 이름 변경 과정에서 오류가 발생했습니다.', details: err });
+      });
+  });
+}
+
+exports.password = (req, res) => {
+  const { token, password, new_password } = req.body;
+  if (!token || !password || !new_password)
+    return res.status(400).json({ error: '필수 파라미터 값이 누락되었습니다.' });
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err)
+      return res.status(401).json({ error: '유효하지 않은 토큰입니다.', details: err });
+
+    const currentHashedPassword = crypto.createHash('sha256').update(password).digest('hex');
+
+    Account.findOne({ user_id: decoded.user_id, password: currentHashedPassword })
+      .then(account => {
+        if (!account)
+          return res.status(401).json({ error: '현재 비밀번호가 일치하지 않습니다.' });
+
+        const newHashedPassword = crypto.createHash('sha256').update(new_password).digest('hex');
+
+        account.password = newHashedPassword;
+        return account.save();
+      })
+      .then(() => {
+        return res.status(200).json({ message: '비밀번호가 성공적으로 변경되었습니다.' });
+      })
+      .catch(err => {
+        if (!res.headersSent)
+          return res.status(500).json({ error: '비밀번호 변경 과정에서 오류가 발생했습니다.', details: err });
       });
   });
 }
