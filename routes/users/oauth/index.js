0811e2f850c2139d98b07a038a999f026418642d
D3vle0 <44903787+D3vle0@users.noreply.github.com>
2024-11-13 18:21:29 +0900

    feat: oauth(google) redirect url

--- routes/users/oauth/index.js
+++ routes/users/oauth/index.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const oauthController = require("./oauth.controller");
 
 router.get("/google", oauthController.google);
+router.get("/google/redirect", oauthController.google_redirect);
 // router.post("/naver", oauthController.naver)
 // router.post("/github", oauthController.github)
 
