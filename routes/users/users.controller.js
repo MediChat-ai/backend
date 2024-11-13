bf5c9a5d00e120d5f80111230a116a84e04c0e63
D3vle0 <44903787+D3vle0@users.noreply.github.com>
2024-11-13 22:00:42 +0900

    fix: status code in /users/register ep

--- routes/users/users.controller.js
+++ routes/users/users.controller.js
@@ -31,7 +31,7 @@ exports.register = (req, res) => {
 	Account.findOne({ user_id })
 		.then(existingAccount => {
 			if (existingAccount)
-				return res.status(400).json({ error: '중복된 ID 입니다.' })
+				return res.status(401).json({ error: '중복된 ID 입니다.' })
 			return new Account({
 				user_id,
 				user_name,
@@ -39,7 +39,7 @@ exports.register = (req, res) => {
 				password: crypto.createHash('sha256').update(pw).digest('hex')
 			}).save();
 		})
-		.then(savedAccount => res.status(201).json({ message: '계정이 생성되었습니다.', account: savedAccount }))
+		.then(savedAccount => res.status(200).json({ message: '계정이 생성되었습니다.', account: savedAccount }))
 		.catch(err => {
 			if (!res.headersSent)
 				return res.status(500).json({ error: '계정 생성 과정에서 오류가 발생했습니다.', details: err });
