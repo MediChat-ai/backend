80a28ae72484fed1c16394c544c77486436cd8f7
정민우 <95672180+minutaur@users.noreply.github.com>
2024-11-13 11:29:10 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,6 @@
-// backend/routes/users/index.js
+const router = require("express").Router();
+const users = require("./users");
 
-const express = require('express');
-const router = express.Router();
-const accountController = require('./users.controller');
-
-// 사용자 관련 라우트
-router.post('/login', accountController.login);
-router.post('/register', accountController.register);
-router.post('/auth', accountController.auth);
-
-// Google OAuth 라우트
-router.get('/auth/google', accountController.googleAuth); // Google 로그인 요청
-router.get('/auth/google/callback', accountController.googleAuthCallback); // Google 로그인 콜백
+router.use("/users", users);
 
 module.exports = router;
