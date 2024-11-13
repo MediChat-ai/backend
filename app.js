83f89b442c82581681a7a143ab87935d1559908b
D3vle0 <44903787+D3vle0@users.noreply.github.com>
2024-11-13 17:23:59 +0900

    randomize session secret

--- app.js
+++ app.js
@@ -5,28 +5,28 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const passport = require('passport');
-const userRoutes = require('./routes/users'); // 사용자 관련 라우터
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const connectDB = require('./db/connect');
+const crypto = require('crypto');
 require('dotenv').config();
 require('./config/passport-setup'); // Passport 설정
 
 const app = express();
 
-// Middleware 설정
 app.use(bodyParser.json());
-app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
+app.use(session({
+    secret: crypto.randomBytes(16).toString('base64'),
+    resave: false, saveUninitialized: true
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
-// MongoDB 연결
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
+connectDB()
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-// 라우터 설정
-app.use('/api/users', userRoutes); // 사용자 라우터 연결
-
-// 서버 실행
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`서버가 ${PORT}번 포트에서 실행 중입니다.`);
 });
