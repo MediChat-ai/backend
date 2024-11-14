22f176f4986b5bbb2df60b563c6b553c0d0e99cc
D3vle0 <44903787+D3vle0@users.noreply.github.com>
2024-11-14 16:22:36 +0900

    fix: return account data without user pw

--- routes/users/users.controller.js
+++ routes/users/users.controller.js
@@ -14,7 +14,8 @@ exports.login = (req, res) => {
 			if (!account)
 				return res.status(401).json({ error: '인증 실패: 잘못된 ID 또는 비밀번호입니다.' });
 			const token = jwt.sign({ user_id: account.user_id, user_name: account.user_name }, process.env.JWT_SECRET, { expiresIn: '24h' });
-			return res.status(200).json({ message: '로그인 성공', token, account });
+			const { password, ...accountWithoutPassword } = account.toObject(); // 비밀번호를 제외한 계정 정보
+			return res.status(200).json({ message: '로그인 성공', token, account: accountWithoutPassword });
 		})
 		.catch(err => {
 			if (!res.headersSent)
@@ -39,7 +40,10 @@ exports.register = (req, res) => {
 				password: crypto.createHash('sha256').update(pw).digest('hex')
 			}).save();
 		})
-		.then(savedAccount => res.status(200).json({ message: '계정이 생성되었습니다.', account: savedAccount }))
+		.then(savedAccount => {
+			const { password, ...accountWithoutPassword } = savedAccount.toObject(); // 비밀번호를 제외한 계정 정보
+			return res.status(200).json({ message: '계정이 생성되었습니다.', account: accountWithoutPassword });
+		})
 		.catch(err => {
 			if (!res.headersSent)
 				return res.status(500).json({ error: '계정 생성 과정에서 오류가 발생했습니다.', details: err });
@@ -59,7 +63,8 @@ exports.auth = (req, res) => {
 				if (!account)
 					return res.status(404).json({ error: '사용자를 찾을 수 없습니다.' });
 
-				return res.status(200).json({ message: '인증 성공', account: account });
+				const { password, ...accountWithoutPassword } = account.toObject(); // 비밀번호를 제외한 계정 정보
+				return res.status(200).json({ message: '인증 성공', account: accountWithoutPassword });
 			})
 			.catch(err => {
 				if (!res.headersSent)
